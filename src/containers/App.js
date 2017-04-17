b82b0e31ffee09000c6725612305529bbeb0184a
Ben Lu <ben@ben-l.com>
2017-04-18 01:20:05 +1200

    Add user explanation to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,10 @@ export default class App extends Component {
   render(){
     return (
       <div>
-        <h1>My App Page</h1>
+        <h1>Server Side Rendering with Create React App v2</h1>
+        <p>Hey, so I've rewritten this example with react-router v4</p>
+        <p>This code is on github: <a href='https://github.com/ayroblu/ssr-creat-react-app-v2'>https://github.com/ayroblu/ssr-creat-react-app-v2</a></p>
+        <p>Medium article: <a href=''></a></p>
         <Switch>
           <Route exact path="/" component={FirstPage}/>
           <Route path="/second" component={SecondPage}/>
