57008a8c288c8ef5c73d7c3a554b4f749ab76e3f
Ben Lu <ben@ben-l.com>
2017-04-18 03:26:09 +1200

    Add in medium article link

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
         <h1>Server Side Rendering with Create React App v2</h1>
         <p>Hey, so I've rewritten this example with react-router v4</p>
         <p>This code is on github: <a href='https://github.com/ayroblu/ssr-create-react-app-v2'>https://github.com/ayroblu/ssr-create-react-app-v2</a></p>
-        <p>Medium article: <a href=''></a></p>
+        <p>Medium article: <a href='https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9'>https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9</a></p>
         <Switch>
           <Route exact path="/" component={FirstPage}/>
           <Route path="/second" component={SecondPage}/>
